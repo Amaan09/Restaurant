f14e109d1909b7de0d7d4926114376f15c51b574
amaan9 <amaan@wishto.co>
2019-08-03 12:02:45 +0530

    made changes

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var options = { promiseLibrary: require('bluebird'), useNewUrlParser: true };
 require('dotenv').config();
 
-// var mongodbUri = process.env.DATABASEURL;
-var mongodbUri = "mongodb://localhost:27017/Restaurant";
-// console.log(process.env.DATABASEURL);
+var mongodbUri = process.env.DATABASEURL;
+// var mongodbUri = "mongodb://localhost:27017/Restaurant";
+console.log(process.env.DATABASEURL);
 mongoose.connect(mongodbUri, options);
 var conn = mongoose.connection;
-console.log(mongodbUri);
+// console.log(mongodbUri);
 
 conn.on('error', console.error.bind(console, 'connection error:'));

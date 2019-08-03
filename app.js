0890965f53af1ba10f1ccb42165b371dac314585
amaan9 <amaan@wishto.co>
2019-08-03 11:37:19 +0530

    db added

---
+++ app.js
@@ -0,0 +1,42 @@
+const   express     = require("express"),
+        app         = express(),
+        db          = require("./db"),
+        jwt         = require("jsonwebtoken"),
+        verifyToken = require("./auth/verifyToken"),
+        key         = require("./key/key"),
+        bodyParser  = require("body-parser");
+
+require('dotenv').config()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// const adminRoutes = require("./admin/adminController");
+
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+});
+
+app.listen(3000,(req,res)=>{
+    console.log("Server Started!!!");
+})

f2c8c3e83924784ea7670f45417b08009a269935
amaan9 <amaan@wishto.co>
2019-08-08 19:58:22 +0530

    made changes

--- Backend/orders/order.js
+++ Backend/orders/order.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-    _id         : mongoose.Schema.Types.ObjectId,
-    menu        : [{
-        type    : mongoose.Schema.Types.ObjectId,
-        ref     : 'Menu'
-    }],
-    quantity    : Number
+    _id         : mongoose.Schema.Types.ObjectId
+
 });
 
 module.exports = mongoose.model('Order', orderSchema);

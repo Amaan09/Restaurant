c7fb540e8b5d75c8a817381114429b0efbe87858
amaan9 <amaan@wishto.co>
2019-08-09 18:52:13 +0530

    made chages

--- Backend/bill/billController.js
+++ Backend/bill/billController.js
@@ -2,3 +2,16 @@ const   express     = require("express"),
         router      = express.Router(),
         mongoose    = require("mongoose"),
         Order       = require("./order");
+
+router.get("/order/:orderId",(req,res,next)=>{
+    Order.findById(req.params.orderId,(err,foundOrder)=>{
+        if(err){
+            res.status(404).send({error: err});
+        } else {
+            res.status(200).send(foundOrder);
+        }
+    })
+});
+        
+
+module.exports = router;

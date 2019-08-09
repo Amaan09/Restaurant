7ecf259d6a228053a08e3d5eb31536b208ac1207
amaan9 <amaan@wishto.co>
2019-08-09 18:48:59 +0530

    made changes

--- Backend/app.js
+++ Backend/app.js
@@ -42,7 +42,8 @@ const   adminRoutes     = require("./admin/adminController"),
         menuRoutes      = require("./menu/menuController"),
         orderRoutes     = require("./orders/orderController"),
         tableRoutes     = require("./tables/tablesController"),
-        kitchenRoutes   = require("./kitchen/kitchenController");      
+        kitchenRoutes   = require("./kitchen/kitchenController"),
+        billRoutes      = require("./bill/billController");      
 
 app.use("/admin",adminRoutes);
 app.use("/login",authRoutes);
@@ -53,6 +54,7 @@ app.use("/menu", verifyToken, menuRoutes);
 app.use("/orders", verifyToken, orderRoutes);
 app.use("/tables", verifyToken, tableRoutes);
 app.use("/kitchen",verifyToken,kitchenRoutes);
+app.use("/bill",verifyToken,billRoutes);
 
 app.listen(3000,(req,res)=>{
     console.log("Server Started!!!");

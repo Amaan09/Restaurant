449b9e5d827215f9964f1a97840a64dffd52c099
amaan9 <amaan@wishto.co>
2019-08-09 19:27:19 +0530

    made changes

--- Backend/bill/billController.js
+++ Backend/bill/billController.js
@@ -2,17 +2,12 @@ const   express     = require("express"),
         router      = express.Router(),
         mongoose    = require("mongoose"),
         Order       = require("../orders/order"),
+        Bill        = require("./bill"),
         moment      = require("moment");
 
-router.get("/order/:orderId",(req,res,next)=>{
-    Order.findById(req.params.orderId,(err,foundOrder)=>{
-        if(err){
-            res.status(404).send({error: err});
-        } else {
-            res.status(200).send(foundOrder);
-        }
-    })
-});
+// router.get("/order/:orderId",(req,res,next)=>{
+
+// });
         
 
 module.exports = router;
